265a666452391729aeb730a2aff880e4c5324525
yyu878567-crypto <yyu878567@gmail.com>
2026-05-23 10:24:36 +0800

    Add files via upload

---
+++ BioData.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hello, I'm YYC")
+
+st.header("About Me")
+
+st.image(image=, caption="A beautiful landscape", use_column_width=True)
+
+st.text(" I've been studying in Python for over 1 month.")
+st.text(" I love going shopping.")
+st.text(" In my free time, I enjoy hiking and reading novels.")
+
+st.success("Welcome to my first Streamlit app!")
